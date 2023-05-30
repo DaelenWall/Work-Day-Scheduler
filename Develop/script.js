19626ec5958bfdb0d98212bb6201e4d2fb7a7d65
Daelen Wall <daelenwall1@gmail.com>
2023-05-30 16:21:22 -0400

    adding click function that save event text to local storage + each function that displays event values.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
-  
-  // Parsing current time
-    var currentTime = parseInt(dayjs().format('H'));
 
-});  
+  // Saving user inputed events to local storage and 
+  $('.saveBtn').click(function () {
+    var eventText = $(this).siblings('.description').val();
+    localStorage.setItem($(this).parent().attr('id'), eventText);
+  })
+});
+
+// Get user input from localStorage and set description values
+$('.time-block').each(function () {
+  var hour = $(this).attr('id');
+  $('#' + hour).find('.description').val(localStorage.getItem(hour));
+
+  // Parsing current time
+  // var currentTime = parseInt(dayjs().format('H'));
+
+});
+
